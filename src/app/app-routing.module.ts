c02035b9d93262edbf9f0879e754ba80674fea78
swust-xiaoj <swustxiaojie@163.com>
2018-04-28 19:16:30 +0800

    feat: add dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HerosComponent } from './heros/heros.component';
-
+import { DashboardComponent } from './dashboard/dashboard.component';
 const routes: Routes = [
-  { path: 'heros', component: HerosComponent }
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'heros', component: HerosComponent },
+  { path: 'dashboard', component: DashboardComponent }
 ];
 
 @NgModule({
+  // The `forRoot()` method supplies the service providers and directives needed for routing,
+  // and performs the initial navigation based on the current browser URL.
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
